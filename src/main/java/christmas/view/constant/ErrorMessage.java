23dbb6d56c3ab99fa469147c902f2e98c780a85c
ffolabear <ffolabear@gmail.com>
2023-11-13 01:44:20 +0900

    update : 에러 메세지 수정

--- src/main/java/christmas/view/constant/ErrorMessage.java
+++ src/main/java/christmas/view/constant/ErrorMessage.java
@@ -2,13 +2,14 @@ package christmas.view.constant;
 
 public enum ErrorMessage {
 
-    ERROR_HEAD("[ERROR]"),
+    ERROR_HEAD("[ERROR] "),
     RE_ENTER(" 다시 입력해 주세요."),
     INVALID_DATE("유효하지 않은 날짜입니다."),
     INVALID_MENU("유효하지 않은 주문입니다."),
     MENU_NOT_EXIST("유효하지 않은 주문입니다."),
     DUPLICATE_MENU("유효하지 않은 주문입니다."),
-    ORDER_EXCEEDED("메뉴는 한 번에 최대 %d개까지만 주문할 수 있습니다.");
+    ORDER_EXCEEDED("메뉴는 한 번에 최대 %d개까지만 주문할 수 있습니다."),
+    MENU_NOT_FOUND("존재하지 않는 매뉴입니다.");
 
     private final String error;
 
