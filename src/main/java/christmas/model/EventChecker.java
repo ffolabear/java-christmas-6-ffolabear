d66bea7b5dc4dd1eb3dd4657832161acb524721e
ffolabear <ffolabear@gmail.com>
2023-11-15 18:29:37 +0900

    feat : 사용자의 주문에 적용가능한 이벤트를 체크하는 기능 구현

---
+++ src/main/java/christmas/model/EventChecker.java
@@ -0,0 +1,37 @@
+package christmas.model;
+
+import christmas.model.domain.Order;
+import christmas.model.domain.event.ChristmasEvent;
+import christmas.model.domain.event.EventApplyResponse;
+import christmas.model.domain.event.GiveawayEvent;
+import christmas.model.domain.event.SpecialEvent;
+import christmas.model.domain.event.WeekDayEvent;
+import christmas.model.domain.event.WeekendEvent;
+import christmas.model.domain.event.eventStructure.Event;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class EventChecker {
+
+    private final List<Event> discountEvents;
+    private static final int EVENT_THRESHOLD = 10_000;
+
+    public EventChecker() {
+        this.discountEvents = Arrays.asList(
+                new WeekDayEvent(), new WeekendEvent(), new GiveawayEvent(), new ChristmasEvent(), new SpecialEvent());
+    }
+
+    public List<EventApplyResponse> checkOrder(Order order) {
+        if (order.getTotalPrice() < EVENT_THRESHOLD) {
+            return new ArrayList<>();
+        }
+        return discountEvents.stream()
+                .filter(event -> event.isApplicable(order))
+                .map(Event::applyEvent)
+                .sorted(Comparator.comparingInt(response -> response.event().ordinal()))
+                .toList();
+    }
+
+}
